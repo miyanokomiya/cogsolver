3466db6609ec432380553ef4b774db0543f151f3
miyanokomiya <tomoya.hstexze24632@gmail.com>
2025-05-18 23:14:15 +0900

    fix: Replace levels

--- src/levels/introductions/Intro_02.ts
+++ src/levels/introductions/Intro_02.ts
@@ -5,9 +5,9 @@ export class Intro_02 extends LevelBase {
   protected setupLevel() {
     this.gearMapComponent.setInitialGears([createGearModel("p-2", "init-1", 100, 500, 1)]);
     this.gearMapComponent.setGoalGears([
-      createGearModel("p-2", "goal-1", 615.873935394876, 177.5266055622622, -1, 1 / 2),
-      createGearModel("p-3", "goal-2", 288.20027047856905, 375.7997295214309, -1, 1 / 2),
-      createGearModel("p-2", "goal-3", 330.27312395916863, 242.2268760408313, -1, 1 / 2),
+      createGearModel("p-2", "goal-1", 627.4005409571382, 344.5, -1, 1 / 2),
+      createGearModel("p-2", "goal-2", 255.49999999999997, 344.5, 1),
+      createGearModel("p-2", "goal-3", 411.70027047856905, 279.7997295214309, 1),
     ]);
     this.gearMapComponent.setAvailableGears([
       ...Array.from({ length: 5 }, () => createGearModel("p-2", Phaser.Utils.String.UUID(), 0, 0)),
